89c099ff107d58c605e3d7dd80c004b77d4773a3
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-08-19 11:16:58 -0700

    Add files via upload

---
+++ Basic_JavaScript_5.js
@@ -0,0 +1,26 @@
+Document.write('typeof 3');
+Document.write(-4E423); 
+Document.write(4E310);
+Document.write(10 > 2);
+Document.write(10 < 2);
+Console.log(2 + 2);
+Document.write("10" + 5);
+Document.write(10 == 10);
+Document.write(10 == 2);
+Document.write(10 === 10);
+Document.write(3 === 11);
+X = 10;
+Y = 10;
+Document.write(X === Y);
+X = 82;
+Y = 5;
+Document.write(X === Y);
+Document.write(5 > 2 && 10 > 4);
+Document.write(5 > 10 && 10 > 4);
+Document.write(5 > 10 || 10 > 4);
+Document.write(5 > 10 || 10 > 20):
+Function not_Function() {
+    Document.getElementById("Not").innerHTML !(20>10);
+}Function not_Function() {
+    Document.getElementById("Not").innerHTML !(5>10);
+}
