41d155c3047b9afe97f7a3655ad7b22f8205f746
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-12-07 12:07:05 -0800

    main.js

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+window.alert("Hello, World!");
+
+var A = "This is a string ";
+document.write (A);
+
+document.write('Hello, World!');
+
+var B = "Welcome to my second string ";
+document.write (B);
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.");
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who don\'t mind.\""
++"-Dr.Seuss ");
+
+var Family = "The Russo", Dad = "Sergio", Mom = "Bellissa", Daughter = "Emilia", Son = "Gian";
+document.write(Dad);
+
+var blues ="I have the blues.";
+var blues =blues.fontcolor("blue");
+document.write(blues);
+
+document.write(4 + 4);
