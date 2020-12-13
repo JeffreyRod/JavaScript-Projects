2b64a4f783cd1d1a75eaf01e49857a22b80b1c88
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-12-12 16:36:38 -0800

    project2js

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() {    //Defining my function. Over in HTMl this will be our element.
+    var sentence = "I am learning"; // Our var sentence //Defining my variable, giving it a string value. The written text is what we want to display.
+    sentence += " a lot from this book!";  // The += will work to concatenate a string With our var sentence.
+    document.getElementById("Concatenate").innerHTML = sentence;  //gathering the value into our HTML element.We will use the concatenate string to join our var sentence and += operator together.
 }
