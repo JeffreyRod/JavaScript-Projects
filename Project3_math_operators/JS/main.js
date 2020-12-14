96a103d367e854a3667dff0d3bac19469acf1786
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-12-14 14:13:30 -0800

    project3

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,49 +1,50 @@
+//SUBTRACTION
 function subtraction_Function() {
-    var subtraction = 5-2;
-    document.getElementById("Math").innerHTML = "5 - 2 =" + subtraction;
+    var subtraction = 5-2;          // Operation we want to exacute.
+    document.getElementById("Math").innerHTML = "5 - 2 =" + subtraction;  // The numbers in quotations will appear when we click the box along with the answer.
 }
-
+//MULTIPLICATION
 function multiplication() {
-    var simple_Math = 6 * 8 ;
-    document.getElementById("Math2").innerHTML = "6 * 8 = " + simple_Math;
+    var simple_Math = 6 * 8 ;    // here we have the operation we want to exacute.
+    document.getElementById("Math2").innerHTML = "6 * 8 = " + simple_Math;  // The numbers in quotations will appear when we click the box along with the answer.
 }
-
+//DIVISION
 function division() {
-    var simple_Math = 48 / 6 ;
-    document.getElementById("Math3").innerHTML = "48 / 6 = " + simple_Math;
+    var simple_Math = 48 / 6 ; // here we have the operation we want to exacute.   
+    document.getElementById("Math3").innerHTML = "48 / 6 = " + simple_Math;  // The numbers in quotations will appear when we click the box along with the answer.
 }
 
 //MULTIPLE OPERATIONS
-function more_Math() {
-    var simple_Math = (1 + 2) * 10 / 2 -5;
+function more_Math() { 
+    var simple_Math = (1 + 2) * 10 / 2 -5; // here we have a combination, A order of operations.
     document.getElementById("Math4").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
 
 //MODULUS OPERATOR
-function modulus_Operator() {
+function modulus_Operator() {  // this will return the remainder.
     var simple_Math = 25 % 6;
     document.getElementById("Math5").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
 }
 
 //UNARY OPERATOR
-function negation_Operator() {
-    var X = 10;
-    document.getElementById("Math6").innerHTML = -X;
+function negation_Operator() {  // a nagation operation will return the oposite or negative of something.
+    var X = 10;                
+    document.getElementById("Math6").innerHTML = -X; // This will return -10
 }
 
 //INCREMENT OPERATOR
-function increment_Operator() {
+function increment_Operator() {   
 var X = 5;
-X++;
-document.getElementById("Math7").innerHTML = "X = 5:" + X++ ;
+X++;                       //this operation will count up one step.
+document.getElementById("Math7").innerHTML = "X = 5:" + X++ ;  // the result will show number 6
 }
 
 //DECREMENT OPERATOR
-function decrement_Operator() {
+function decrement_Operator() {    
 var X = 5.25;
-X--
-document.getElementById("Math8").innerHTML = "X=5.25:" + X-- ;
+X--                       //this operation will decrease.
+document.getElementById("Math8").innerHTML = "X=5.25:" + X-- ;  // result will be 4.25
 }
 
-// Random window alert. A random number will pop up between 0 and 100
+// Random window alert. A random number will pop up between 0 and 100 when opening web page.
 window.alert(Math.random() * 100);
