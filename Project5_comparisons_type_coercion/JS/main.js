6082f62a4093d7e5bfa8e1daecafbacc68f9a095
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-12-19 11:39:06 -0800

    Project5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,17 @@
+//"TYPE OF" OPERATOR
+
+document.write(typeof "Font");  // The word "Font" is a String.
+
+document.write(typeof 3);  //The Number "3" is a Number.
+ 
+document.write(2E310);   //Displaying "infinity" we write a high floating point number.
+
+function my_Function() {
+   var x = 5e23875;
+   var y = -9e2898735;
+   document.getElementById("Test").innerHTML = x + "<br>" + y;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = 10 > 9;
+}
