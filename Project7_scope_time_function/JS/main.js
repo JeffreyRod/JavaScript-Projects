1e23efd1aafa5dac63500c8c8de3c0f99603607f
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-12-22 15:04:39 -0800

    Project 7

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,4 +1,3 @@
-
 //A global variable would be written as follows:
 var X = 10;
 function Add_numbers_1() {
@@ -10,28 +9,26 @@ function Add_numbers_2() {
 Add_numbers_1 () ;
 Add_numbers_2 () ;
 
-document.write("<br>");
 
 //LOCAL VARIABLE
-function Add_numbers_1() {
+function Add_numbers_3() {
     var X = 10;
     document.write(20 + X + "<br>");
 }
-function Add_numbers_2() {
+function Add_numbers_4() {
     document.write(X + 100);
     }
-Add_numbers_1 ();
-Add_numbers_2 ();
+Add_numbers_3 ();
+Add_numbers_4 ();
 
-document.write("<br>");
 
 //console.log() method, if you open the console, you’ll see the error “X is not defined.”
-function Add_numbers_1() {
+function Add_numbers_5() {
     var X = 10;
     console.log(15 + X);
 }
-function Add_numbers_2() {
+function Add_numbers_6() {
     console.log(X + 100);
 }
-Add_numbers_1() ;
-Add_numbers_2() ;
+Add_numbers_5() ;
+Add_numbers_6() ;
