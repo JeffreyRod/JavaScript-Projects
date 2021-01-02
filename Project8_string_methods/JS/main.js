13dcac4b9c21b0b30b6550487f11f518510342f5
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2021-01-02 05:17:39 -0800

    Project8

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,28 @@
+//CONCAT() method. Is connectimg two or more strings.
+function full_Sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4) ;
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+//SLICE() METHOD,extracts a section of a string and then returns the extracted section in a new string.
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+//NUMBER METHODS
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();  // “toString()” will return the number as a string.
+}
+
+//TOPRECISION() METHOD will specify a number length for the output
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10) ;
+}
