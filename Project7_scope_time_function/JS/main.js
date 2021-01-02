fdc51353032f1a660f9248cd89476d49d72370ef
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2021-01-02 04:11:29 -0800

    project7

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,6 +1,6 @@
 //A global variable would be written as follows:
-var X = 10;
 function Add_numbers_1() {
+    var X = 10;
     document.write(20 + X + "<br>");
 }
 function Add_numbers_2() {
@@ -32,3 +32,38 @@ function Add_numbers_6() {
 }
 Add_numbers_5() ;
 Add_numbers_6() ;
+
+//IF STATEMENTS
+function get_Date(){
+    if (new Date().getHours() < 18) {  //Date().getHours() method returns the hour in the specified date according to the local time, and the hours
+    document.getElementById("Greeting").innerHTML = "How are you today";
+    }
+}
+
+// ELSE STATEMENTS
+function Age_Function() {
+    Age = document.getElementById("Age").valyue;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+//ELSE IF STATEMENTS
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It's morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It's the afternoon.";
+    }
+    else {
+        Reply = "It's evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
