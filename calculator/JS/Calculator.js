f990ebe809259fb4ffeb929df64de06b9b8877aa
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2021-05-26 08:06:02 -0700

    calculator.js

---
+++ calculator/JS/Calculator.js
@@ -0,0 +1,11 @@
+// creates an object to keep track of values
+const Calculator = {
+    // This is displays 0 on the screen
+    Display_Value: '0',
+    // This will hold the first operand for any expressions, we set it to null for now 
+    First_Operand: null,
+    // This checks whether or not the second operand has been input
+    Wait_Second_Operand: false,
+    // This will hold the operator, we set it to null for now 
+    operator: null,
+};
